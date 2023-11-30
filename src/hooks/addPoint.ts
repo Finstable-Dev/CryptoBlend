a3b811c700fffc0759d6608cf674c93067765596
waritsara <warissara.0039@gmail.com>
2023-11-30 10:26:21 +0700

    update typechain

--- src/hooks/addPoint.ts
+++ src/hooks/addPoint.ts
@@ -1,12 +1,53 @@
 import { addressList } from "@/constants/addressList";
+import { IMetadata } from "@/interfaces/campaign.interface";
+import readMetadataService from "@/services/readMetadata.service";
 import { CryptoCoffPoint__factory } from "@/typechain-types";
-import { useContractWrite } from "wagmi";
+import { useMemo } from "react";
+import { useContractRead, useAccount, useContractReads } from "wagmi";
 
-export function useAddPoint(address: `0x${string}`, point: number) {
-  const { write } = useContractWrite({
+export function useGetSuccessToken() {
+  const { address } = useAccount();
+  const { data } = useContractRead({
     address: addressList.CryptoCoffPoint,
     abi: CryptoCoffPoint__factory.abi,
-    functionName: "addPoint",
-    args: [address, BigInt(point)],
+    functionName: "getTokenOfOwnerByIndex",
+    args: [address!],
+    enabled: !!address,
   });
+
+  const tokenUriContract = {
+    address: addressList.CryptoCoffPoint,
+    abi: CryptoCoffPoint__factory.abi,
+    functionName: "tokenURI",
+  };
+
+  const tokenIdList = useMemo(() => {
+    if (!data) return [];
+    return data.map((d: BigInt) => Number(d));
+  }, [data]);
+
+  const tokenUriContracts = useMemo(() => {
+    return tokenIdList.map((id) => ({
+      ...tokenUriContract,
+      args: [BigInt(id)],
+    }));
+  }, [tokenIdList]);
+
+  const { data: tokenUriList } = useContractReads({
+    contracts: tokenUriContracts,
+  });
+
+  const tokenDataList = useMemo(() => {
+    const result: IMetadata[] = [];
+    if (!tokenUriList) return [];
+    tokenUriList.forEach(async (item, index) => {
+      const metadata = await readMetadataService.readMetadata(
+        item.result as string
+      );
+      result.push({
+        ...metadata.data,
+        id: tokenIdList[index],
+      });
+    });
+  }, [tokenUriList]);
 }
